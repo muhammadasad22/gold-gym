33804ce0c33d6bbf98441070c83829ff8940c133
Muhammadasad22 <muhammadasad2291@gmail.com>
2022-06-18 16:28:03 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Box>
   );
 };
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -16,7 +16,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       setBodyParts(["all", ...bodyPartsData]);
     };
     fetchExercisesData();
-  });
+  }, []);
 
   const handleSearch = async () => {
     if (search) {
@@ -26,13 +26,14 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       );
 
       const searchExercises = exerciseData.filter(
-        (exercise) =>
-          exercise.name.toLowerCase().includes(search) ||
-          exercise.target.toLowerCase().includes(search) ||
-          exercise.equipment.toLowerCase().includes(search) ||
-          exercise.bodyPart.toLowerCase().includes(search)
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
       );
 
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
       setSearch("");
       setExercises(searchExercises);
     }
